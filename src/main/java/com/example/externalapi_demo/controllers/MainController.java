58ef8d0e2b5f6d74c2a8e5c227a24ec05ead1dbb
Phuoc Bui <phuocbui@kms.com.vn>
2022-11-10 17:39:32 +0700

    ok

--- src/main/java/com/example/externalapi_demo/controllers/MainController.java
+++ src/main/java/com/example/externalapi_demo/controllers/MainController.java
@@ -1,2 +1,47 @@
-package com.example.externalapi_demo.controllers;public class MainController {
+package com.example.externalapi_demo.controllers;
+
+import com.example.externalapi_demo.entities.Activity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
+
+@RestController
+public class MainController {
+    @GetMapping("/hello")
+    public String api1() {
+        return "hello";
+    }
+
+
+    @GetMapping("/api1")
+    public String api2() {
+        String uri = "https://dummy.restapiexample.com/api/v1/employees";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        return result;
+    }
+
+    @GetMapping("/callApi")
+    public int callApi1() {
+        String uri = "https://dummy.restapiexample.com/api/v1/employees";
+        RestTemplate restTemplate = new RestTemplate();
+        Objects[] result =restTemplate.getForObject(uri, Objects[].class);
+
+        return result.length;
+    }
+
+    @GetMapping("/randomActivity")
+    public Activity callApi12() {
+        String uri = "https://www.boredapi.com/api/activity";
+        RestTemplate restTemplate = new RestTemplate();
+//        Objects result = restTemplate.getForObject(uri, String.class);
+        Activity result = restTemplate.getForObject(uri, Activity.class);
+        System.out.println(result.toString());
+        return result;
+    }
+
+
+
 }
--- src/main/java/com/example/externalapi_demo/entities/Activity.java
+++ src/main/java/com/example/externalapi_demo/entities/Activity.java
@@ -1,2 +1,22 @@
-package com.example.externalapi_demo.entities;public class Activity {
+package com.example.externalapi_demo.entities;
+
+import lombok.Data;
+
+@Data
+public class Activity {
+    private String activity;
+    private String type;
+//    private int participants;
+//    private float price;
+//    private String link;
+//    private String key;
+//    private float accessibility;
+
+    @Override
+    public String toString() {
+        return "Activity{" +
+                "activity='" + activity + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
 }
